e66adce12ae3621f461049e5891cb0a05fa37823
Mikhail Vezhnovets <mvezhnovets@gmail.com>
2022-12-18 19:20:42 +0300

    add work

---
+++ dist/index.579125c3.js
@@ -0,0 +1,23 @@
+new Swiper(".made-slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    loop: true
+});
+new Swiper(".news-slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    loop: true
+});
+new Swiper(".rewiev-content ", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    loop: true
+});
+
+//# sourceMappingURL=index.579125c3.js.map
---
+++ src/index.js
@@ -0,0 +1,22 @@
+new Swiper('.made-slider', {
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+})
+new Swiper('.news-slider', {
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+})
+new Swiper('.rewiev-content ', {
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   loop: true,
+})
+
